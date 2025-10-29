71ee304b063c2a00622805826d78098729819128
Daniel Soldatov <danielsoldatov140208@gmail.com>
2025-10-29 16:06:43 +0500

    начал работу над регистрированием

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from . import views
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('user/', views.user_profile, name='user'),
     path('sale/', views.sale, name='sale'),
+    path('profile/<str:username>/', views.user_profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('register/', views.register_view, name='register'),
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 
 def home(request):
     return render(request, 'main.html')
 
 def sale(request):
     return render(request, 'sale.html')
+
+@login_required
+def user_profile(request, username):
+    user_obj = get_object_or_404(User, username=username)
+    return render(request, 'profile.html', {'user_obj': user_obj})
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user) 
+            return redirect('home') 
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
